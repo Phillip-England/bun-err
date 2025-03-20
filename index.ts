78e7a55d3e4e28d1c831e98f81253cb048aeee4a
Phillip England <phillip.mark.england@gmail.com>
2025-03-20 05:12:30 -0500

    testing out rust-like result type

--- index.ts
+++ index.ts
@@ -1,10 +1,48 @@
-export type Result<T> = [T | null, Error | null];
+type ResultType<T> = {
+  isOk(): this is Ok<T>;
+  isErr(): this is Err<T>;
+  unwrap(): T | Promise<T>;
+  unwrapErr(): Error;
+};
 
-export async function go<T>(callback: () => Promise<T>): Promise<[T | null, Error | null]> {
-  try {
-    const val = await callback();
-    return [val, null];
-  } catch (err) {
-    return [null, err as Error];
+class Ok<T> implements ResultType<T> {
+  constructor(private value: T | Promise<T>) {}
+  isOk(): this is Ok<T> {
+    return true;
+  }
+  isErr(): this is Err<T> {
+    return false;
+  }
+  unwrap(): T | Promise<T> {
+    return this.value;
+  }
+  unwrapErr(): Error {
+    throw new Error("Called unwrapErr on Ok");
   }
 }
+
+class Err<T> implements ResultType<T> {
+  constructor(private error: Error) {}
+  isOk(): this is Ok<T> {
+    return false;
+  }
+  isErr(): this is Err<T> {
+    return true;
+  }
+  unwrap(): T | Promise<T> {
+    throw this.error;
+  }
+  unwrapErr(): Error {
+    return this.error;
+  }
+}
+
+export const Result = {
+  ok<T>(value: T | Promise<T>): ResultType<T> {
+    return new Ok<T>(value);
+  },
+  err<T = unknown>(error: Error | string): ResultType<T> {
+    const errInstance = typeof error === 'string' ? new Error(error) : error;
+    return new Err<T>(errInstance);
+  }
+};
